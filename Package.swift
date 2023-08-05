8bc7241eb5a25a6c3644dc8e202e16d5e0344b44
Vishwa Patel <vishwa.patel@medibank.com.au>
2023-08-05 19:22:32 +1000

    Removed Lottie

--- Package.swift
+++ Package.swift
@@ -24,6 +24,6 @@ let package = Package(
               checksum: "335ccbceaff27fef4d95fc65239fd3722f1646dc34519c43dc3b5a852b446a33"),
         .target(
             name: "BinaryOnlyPackage",
-            dependencies: ["Lottie", "MyTestFramework"])
+            dependencies: [ "MyTestFramework"])
     ]
 )
