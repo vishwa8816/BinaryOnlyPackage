a24ce82a3d0eb22b17c9192c1c11a521b3ec0b25
Vishwa Patel <vishwa.patel@medibank.com.au>
2023-08-05 19:29:17 +1000

    Updated Package

--- Package.swift
+++ Package.swift
@@ -12,18 +12,17 @@ let package = Package(
             targets: ["BinaryOnlyPackage"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.2.0")
     ],
     targets: [
-        .binaryTarget(
-              name: "Lottie",
-              url: "https://github.com/airbnb/lottie-ios/releases/download/4.2.0/Lottie.xcframework.zip",
-              checksum: "4db3dee208f6213e5c1681f2314c7ed96d466d9b9adfe5cd0030309515075443"),
         .binaryTarget(
               name: "MyTestFramework",
               url: "https://github.com/vishwa8816/MyTestFrameworkA/releases/download/Third/MyTestFramework.xcframework.zip",
               checksum: "335ccbceaff27fef4d95fc65239fd3722f1646dc34519c43dc3b5a852b446a33"),
         .target(
             name: "BinaryOnlyPackage",
-            dependencies: [ "MyTestFramework"])
+            dependencies: ["MyTestFramework",
+                           .product(name: "Lottie", package: "lottie-spm")
+            ])
     ]
 )
